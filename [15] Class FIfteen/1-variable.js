ec11b03ee3651140163cd7dc25c53006cd4b52e2
Sudarshan Amatya <sudarshanamatya1122@gmail.com>
2025-12-01 09:07:15 +0545

    HELLO

--- [15] Class FIfteen/1-variable.js
+++ [15] Class FIfteen/1-variable.js
@@ -4,3 +4,11 @@ console.log("HELLO WORLD.")
  */
 let name = "Sudarshan Amatya";
 console.log(name);
+let courses = [
+    {
+        name: "mern",
+        price: 10
+    },
+    "python",
+    "javascript"]
+console.log(courses)
