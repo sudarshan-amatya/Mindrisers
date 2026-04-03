88625870bd050f62ddba8e97b2ec647210459c89
Sudarshan Amatya <sudarshanamatya1122@gmail.com>
2026-04-03 12:47:22 +0545

    Delete social-web-app directory

--- social-web-app/server/prisma/migrations/20260401150431_add_message_read_state/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "Message" ADD COLUMN     "isRead" BOOLEAN NOT NULL DEFAULT false,
-ADD COLUMN     "readAt" TIMESTAMP(3);
--- social-web-app/server/prisma/migrations/20260401162548_add_reposts/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "Post" ADD COLUMN     "repostOfId" TEXT;
-
--- CreateIndex
-CREATE INDEX "Post_repostOfId_idx" ON "Post"("repostOfId");
-
--- AddForeignKey
-ALTER TABLE "Post" ADD CONSTRAINT "Post_repostOfId_fkey" FOREIGN KEY ("repostOfId") REFERENCES "Post"("id") ON DELETE CASCADE ON UPDATE CASCADE;
